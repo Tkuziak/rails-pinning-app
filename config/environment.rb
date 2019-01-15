cc14792bca441717a31f1a94bdc62a2a2be681e1
tkuziak <teresa.kuziak@shaw.ca>
2019-01-15 16:23:42 -0700

    Deploying to Heroku 4

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,5 @@
 # Load the Rails application.
-#require File.expand_path('../application', __FILE__)
-
-# Initialize the Rails application.
-#Rails.application.initialize!
-#Try the environment.rb from the AWS site
-
-
-# Load the Rails application.
-require_relative 'application'
+require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
